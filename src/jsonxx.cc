71c9f114f37f3e9450aa8b73c834da0c83094dc0
Saleem Abdulrasool <compnerd@compnerd.org>
2015-11-15 15:27:00 -0800

    reformat using clang-format; update modelines

--- src/jsonxx.cc
+++ src/jsonxx.cc
@@ -1,33 +1,32 @@
-/* vim: set et fdm=syntax sts=4 sw=4 ts=4 : */
+/* vim: set et fdm=syntax sts=2 sw=2 ts=8 : */
 /**
- * Copyright © 2010,2012 Saleem Abdulrasool <compnerd@compnerd.org>
+ * Copyright © 2010,2012,2015 Saleem Abdulrasool <compnerd@compnerd.org>
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation and/or
- *    other materials provided with the distribution.
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
  *
- * 3. The name of the author may not be used to endorse or promote products derived
- *    from this software without specific prior written permission.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
- * OF SUCH DAMAGE.
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **/
-
 
 #include <string.h>
 #include <strings.h>
@@ -39,302 +38,303 @@
 
 namespace jsonxx {
 namespace internals {
-struct whitespace_traits { bool allow_comments; };
+struct whitespace_traits {
+  bool allow_comments;
+};
 whitespace_traits whitespace = { 0 };
 
-std::istream & operator>>(std::istream& stream, whitespace_traits&) {
-    char ch;
+std::istream& operator>>(std::istream& stream, whitespace_traits&) {
+  char ch;
 
-    stream >> std::ws;
+  stream >> std::ws;
 
-    if (!stream)
-        return stream;
+  if (!stream)
+    return stream;
 
-    if (stream.peek() != '/')
-        return stream;
+  if (stream.peek() != '/')
+    return stream;
 
-    stream.get(ch);
+  stream.get(ch);
 
-    if (!stream)
-        return stream;
+  if (!stream)
+    return stream;
 
-    switch (stream.peek()) {
-    case '/':
-        while (stream) {
-            stream.get(ch);
-            if (ch == '\n')
-                break;
-        }
-        break;
-    case '*':
-        while (stream) {
-            stream.get(ch);
-            if (ch == '*' && stream.peek() == '/')
-                break;
-        }
-        if (stream)
-            stream.get(ch);
-        break;
-    default:
-        stream.unget();
+  switch (stream.peek()) {
+  case '/':
+    while (stream) {
+      stream.get(ch);
+      if (ch == '\n')
         break;
     }
+    break;
+  case '*':
+    while (stream) {
+      stream.get(ch);
+      if (ch == '*' && stream.peek() == '/')
+        break;
+    }
+    if (stream)
+      stream.get(ch);
+    break;
+  default:
+    stream.unget();
+    break;
+  }
 
-    return stream >> whitespace;
+  return stream >> whitespace;
 }
 
 void consume_string(std::istream& stream, string& value) {
-    std::ostringstream buffer;
-    char ch;
+  std::ostringstream buffer;
+  char ch;
 
+  stream.get(ch);
+  if (ch != '"') {
+    stream.setstate(stream.rdstate() | std::ios_base::failbit);
+    // TODO(abdulras) throw an exception
+    return;
+  }
+
+  while (stream) {
     stream.get(ch);
-    if (ch != '"') {
-        stream.setstate(stream.rdstate() | std::ios_base::failbit);
-        // TODO(abdulras) throw an exception
-        return;
+    if (ch == '"')
+      break;
+
+    switch (ch) {
+    case '\\':
+      stream.get(ch);
+      switch (ch) {
+      case '"':
+      case '\\':
+      case '/':
+        buffer << ch;
+        break;
+      case 'b':
+        buffer << '\b';
+        break;
+      case 'f':
+        buffer << '\f';
+        break;
+      case 'n':
+        buffer << '\n';
+        break;
+      case 'r':
+        buffer << '\r';
+        break;
+      case 't':
+        buffer << '\t';
+        break;
+      case 'u':
+        // TODO(abdulras) implement unicode handling
+        assert(false);
+        break;
+      default:
+        buffer << '\\' << ch;
+        break;
+      }
+      break;
+    default:
+      buffer << ch;
+      break;
     }
+  }
 
-    while (stream) {
-        stream.get(ch);
-        if (ch == '"')
-            break;
+  if (ch != '"') {
+    stream.setstate(stream.rdstate() | std::ios_base::failbit);
+    // TODO(abdulras) throw an exception
+    return;
+  }
 
-        switch (ch) {
-        case '\\':
-            stream.get(ch);
-            switch (ch) {
-            case '"':
-            case '\\':
-            case '/':
-                buffer << ch;
-                break;
-            case 'b':
-                buffer << '\b';
-                break;
-            case 'f':
-                buffer << '\f';
-                break;
-            case 'n':
-                buffer << '\n';
-                break;
-            case 'r':
-                buffer << '\r';
-                break;
-            case 't':
-                buffer << '\t';
-                break;
-            case 'u':
-                // TODO(abdulras) implement unicode handling
-                assert(false);
-                break;
-            default:
-                buffer << '\\' << ch;
-                break;
-            }
-            break;
-        default:
-            buffer << ch;
-            break;
-        }
-    }
-
-    if (ch != '"') {
-        stream.setstate(stream.rdstate() | std::ios_base::failbit);
-        // TODO(abdulras) throw an exception
-        return;
-    }
-
-    value = buffer.str();
+  value = buffer.str();
 }
 
 void consume_number(std::istream& stream, number& value) {
-    stream >> value;
+  stream >> value;
 }
 
 void consume_array(std::istream& stream, array& value) {
-    array array;
-    char ch;
+  array array;
+  char ch;
+
+  stream.get(ch);
+  if (ch != '[') {
+    stream.setstate(stream.rdstate() | std::ios_base::failbit);
+    // TODO(abdulras) throw an exception
+    return;
+  }
+
+  while (stream) {
+    jsonxx::value element;
+
+    if (stream.peek() == ',')
+      stream.get(ch);
+
+    stream >> whitespace;
+
+    if (stream.peek() == ']')
+      break;
+
+    parse(stream, element);
+    array.push_back(element);
+
+    stream >> whitespace;
 
     stream.get(ch);
-    if (ch != '[') {
-        stream.setstate(stream.rdstate() | std::ios_base::failbit);
-        // TODO(abdulras) throw an exception
-        return;
+    if (ch != ',') {
+      stream.unget();
+      break;
     }
+  }
 
-    while (stream) {
-        jsonxx::value element;
+  stream.get(ch);
+  if (ch != ']') {
+    stream.setstate(stream.rdstate() | std::ios_base::failbit);
+    // TODO(abdulras) throw an exception
+    return;
+  }
 
-        if (stream.peek() == ',')
-            stream.get(ch);
-
-        stream >> whitespace;
-
-        if (stream.peek() == ']')
-            break;
-
-        parse(stream, element);
-        array.push_back(element);
-
-        stream >> whitespace;
-
-        stream.get(ch);
-        if (ch != ',') {
-            stream.unget();
-            break;
-        }
-    }
-
-    stream.get(ch);
-    if (ch != ']') {
-        stream.setstate(stream.rdstate() | std::ios_base::failbit);
-        // TODO(abdulras) throw an exception
-        return;
-    }
-
-    value = array;
+  value = array;
 }
 
 void consume_object(std::istream& stream, object& value) {
-    object object;
-    char ch;
+  object object;
+  char ch;
 
-    stream.get(ch);
-    if (ch != '{') {
-        stream.setstate(stream.rdstate() | std::ios_base::failbit);
-        // TODO(abdulras) throw an exception
-        return;
+  stream.get(ch);
+  if (ch != '{') {
+    stream.setstate(stream.rdstate() | std::ios_base::failbit);
+    // TODO(abdulras) throw an exception
+    return;
+  }
+
+  while (stream) {
+    std::string k;
+    jsonxx::value v;
+
+    if (stream.peek() == ',')
+      stream.get(ch);
+
+    stream >> whitespace;
+
+    if (stream.peek() == '}')
+      break;
+
+    consume_string(stream, k);
+    if (!stream) {
+      stream.setstate(stream.rdstate() | std::ios_base::failbit);
+      // TODO(abdulras) throw an exception
+      return;
     }
 
-    while (stream) {
-        std::string k;
-        jsonxx::value v;
-
-        if (stream.peek() == ',')
-            stream.get(ch);
-
-        stream >> whitespace;
-
-        if (stream.peek() == '}')
-            break;
-
-        consume_string(stream, k);
-        if (!stream) {
-            stream.setstate(stream.rdstate() | std::ios_base::failbit);
-            // TODO(abdulras) throw an exception
-            return;
-        }
-
-        stream >> whitespace;
-
-        stream.get(ch);
-        if (ch != ':') {
-            stream.setstate(stream.rdstate() | std::ios_base::failbit);
-            // TODO(abdulras) throw an exception
-            return;
-        }
-
-        parse(stream, v);
-        object[k] = v;
-
-        if (! stream) {
-            stream.setstate(stream.rdstate() | std::ios_base::failbit);
-            // TODO(abdulras) throw an exception
-            return;
-        }
-
-        stream >> whitespace;
-
-        stream.get(ch);
-        if (ch != ',') {
-            stream.unget();
-            break;
-        }
-    }
+    stream >> whitespace;
 
     stream.get(ch);
-    if (ch != '}')
-        stream.setstate(stream.rdstate() | std::ios_base::failbit);
+    if (ch != ':') {
+      stream.setstate(stream.rdstate() | std::ios_base::failbit);
+      // TODO(abdulras) throw an exception
+      return;
+    }
 
-    value = object;
+    parse(stream, v);
+    object[k] = v;
+
+    if (!stream) {
+      stream.setstate(stream.rdstate() | std::ios_base::failbit);
+      // TODO(abdulras) throw an exception
+      return;
+    }
+
+    stream >> whitespace;
+
+    stream.get(ch);
+    if (ch != ',') {
+      stream.unget();
+      break;
+    }
+  }
+
+  stream.get(ch);
+  if (ch != '}')
+    stream.setstate(stream.rdstate() | std::ios_base::failbit);
+
+  value = object;
 }
 
 void consume_boolean(std::istream& stream, boolean& value) {
-    char buffer[6] = {0};
+  char buffer[6] = { 0 };
 
-    stream.read(buffer, 5);
+  stream.read(buffer, 5);
 
-    if (::strncmp(buffer, "true", 4) == 0) {
-        stream.unget();
-        value = true;
-    } else if (::strcmp(buffer, "false") == 0) {
-        value = false;
-    } else {
-        stream.setstate(stream.rdstate() | std::ios_base::failbit);
-    }
+  if (::strncmp(buffer, "true", 4) == 0) {
+    stream.unget();
+    value = true;
+  } else if (::strcmp(buffer, "false") == 0) {
+    value = false;
+  } else {
+    stream.setstate(stream.rdstate() | std::ios_base::failbit);
+  }
 }
 
 void consume_null(std::istream& stream, null&) {
-    char buffer[5] = {0};
+  char buffer[5] = { 0 };
 
-    stream.read(buffer, 4);
+  stream.read(buffer, 4);
 
-    if (::strcmp(buffer, "null") != 0)
-        stream.setstate(stream.rdstate() | std::ios_base::failbit);
+  if (::strcmp(buffer, "null") != 0)
+    stream.setstate(stream.rdstate() | std::ios_base::failbit);
 }
 }
 
 void allow_comments(const bool value) {
-    internals::whitespace.allow_comments = value;
+  internals::whitespace.allow_comments = value;
 }
 
 bool allow_comments() {
-    return internals::whitespace.allow_comments;
+  return internals::whitespace.allow_comments;
 }
 
 void parse(std::istream& stream, value& value) {
-    char ch;
+  char ch;
 
-    stream >> internals::whitespace;
+  stream >> internals::whitespace;
 
-    stream.get(ch);
-    switch (ch) {
-    case '{':
-        internals::consume_object(stream.unget(), value.object_);
-        if (!stream.fail())
-            value.type_ = value::object;
-        break;
-    case '[':
-        internals::consume_array(stream.unget(), value.array_);
-        if (!stream.fail())
-            value.type_ = value::array;
-        break;
-    case '"':
-        internals::consume_string(stream.unget(), value.string_);
-        if (!stream.fail())
-            value.type_ = value::string;
-        break;
-    case '-':
-    case '0' ... '9':
-        internals::consume_number(stream.unget(), value.number_);
-        if (!stream.fail())
-            value.type_ = value::number;
-        break;
-    case 't':
-    case 'f':
-        internals::consume_boolean(stream.unget(), value.boolean_);
-        if (!stream.fail())
-            value.type_ = value::boolean;
-        break;
-    case 'n':
-        internals::consume_null(stream.unget(), value.null_);
-        if (!stream.fail())
-            value.type_ = value::null;
-        break;
-    default:
-        // throw
-        break;
-    }
+  stream.get(ch);
+  switch (ch) {
+  case '{':
+    internals::consume_object(stream.unget(), value.object_);
+    if (!stream.fail())
+      value.type_ = value::object;
+    break;
+  case '[':
+    internals::consume_array(stream.unget(), value.array_);
+    if (!stream.fail())
+      value.type_ = value::array;
+    break;
+  case '"':
+    internals::consume_string(stream.unget(), value.string_);
+    if (!stream.fail())
+      value.type_ = value::string;
+    break;
+  case '-':
+  case '0' ... '9':
+    internals::consume_number(stream.unget(), value.number_);
+    if (!stream.fail())
+      value.type_ = value::number;
+    break;
+  case 't':
+  case 'f':
+    internals::consume_boolean(stream.unget(), value.boolean_);
+    if (!stream.fail())
+      value.type_ = value::boolean;
+    break;
+  case 'n':
+    internals::consume_null(stream.unget(), value.null_);
+    if (!stream.fail())
+      value.type_ = value::null;
+    break;
+  default:
+    // throw
+    break;
+  }
 }
 }
-
--- src/jsonxx.hh
+++ src/jsonxx.hh
@@ -1,46 +1,46 @@
-/* vim: set et fdm=syntax sts=4 sw=4 ts=4 : */
+/* vim: set et fdm=syntax sts=2 sw=2 ts=8 : */
 /**
- * Copyright © 2010,2012 Saleem Abdulrasool <compnerd@compnerd.org>
+ * Copyright © 2010,2012,2015 Saleem Abdulrasool <compnerd@compnerd.org>
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation and/or
- *    other materials provided with the distribution.
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
  *
- * 3. The name of the author may not be used to endorse or promote products derived
- *    from this software without specific prior written permission.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
- * OF SUCH DAMAGE.
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **/
 
 #ifndef jsonxx_jsonxx_hh_
 #define jsonxx_jsonxx_hh_
 
+#include <cassert>
+#include <iostream>
 #include <map>
 #include <string>
 #include <vector>
-#include <cassert>
-#include <iostream>
 
 namespace jsonxx {
 class value;
 
-struct null { };
+struct null {};
 typedef bool boolean;
 typedef double number;
 typedef std::string string;
@@ -50,92 +50,104 @@ typedef std::map<std::string, value> object;
 class value {
   friend void parse(std::istream&, value&);
 
-  public:
-    template <typename type_>
-    bool is() const;
+public:
+  template <typename type_>
+  bool is() const;
 
-    template <typename type_>
-    type_& as();
+  template <typename type_>
+  type_& as();
 
-  private:
-    enum value_type {
-        invalid,
-        string,
-        number,
-        object,
-        array,
-        boolean,
-        null,
-    };
+private:
+  enum value_type {
+    invalid,
+    string,
+    number,
+    object,
+    array,
+    boolean,
+    null,
+  };
 
-    jsonxx::string string_;
-    jsonxx::number number_;
-    jsonxx::object object_;
-    jsonxx::array array_;
-    jsonxx::boolean boolean_;
-    jsonxx::null null_;
+  jsonxx::string string_;
+  jsonxx::number number_;
+  jsonxx::object object_;
+  jsonxx::array array_;
+  jsonxx::boolean boolean_;
+  jsonxx::null null_;
 
-    value_type type_;
+  value_type type_;
 };
 
 template <>
-inline bool value::is<string>() const { return type_ == value::string; }
+inline bool value::is<string>() const {
+  return type_ == value::string;
+}
 
 template <>
-inline bool value::is<number>() const { return type_ == value::number; }
+inline bool value::is<number>() const {
+  return type_ == value::number;
+}
 
 template <>
-inline bool value::is<object>() const { return type_ == value::object; }
+inline bool value::is<object>() const {
+  return type_ == value::object;
+}
 
 template <>
-inline bool value::is<array>() const { return type_ == value::array; }
+inline bool value::is<array>() const {
+  return type_ == value::array;
+}
 
 template <>
-inline bool value::is<boolean>() const { return type_ == value::boolean; }
+inline bool value::is<boolean>() const {
+  return type_ == value::boolean;
+}
 
 template <>
-inline bool value::is<null>() const { return type_ == value::null; }
+inline bool value::is<null>() const {
+  return type_ == value::null;
+}
 
 template <>
 inline string& value::as<jsonxx::string>() {
-    // TODO(abdulras) throw an exception rather than asserting
-    assert(is<jsonxx::string>());
-    return string_;
+  // TODO(abdulras) throw an exception rather than asserting
+  assert(is<jsonxx::string>());
+  return string_;
 }
 
 template <>
 inline number& value::as<jsonxx::number>() {
-    // TODO(abdulras) throw an exception rather than asserting
-    assert(is<jsonxx::number>());
-    return number_;
+  // TODO(abdulras) throw an exception rather than asserting
+  assert(is<jsonxx::number>());
+  return number_;
 }
 
 template <>
 inline object& value::as<jsonxx::object>() {
-    // TODO(abdulras) throw an exception rather than asserting
-    assert(is<jsonxx::object>());
-    return object_;
+  // TODO(abdulras) throw an exception rather than asserting
+  assert(is<jsonxx::object>());
+  return object_;
 }
 
 template <>
 inline array& value::as<jsonxx::array>() {
-    // TODO(abdulras) throw an exception rather than asserting
-    assert(is<jsonxx::array>());
-    return array_;
+  // TODO(abdulras) throw an exception rather than asserting
+  assert(is<jsonxx::array>());
+  return array_;
 }
 
 template <>
 inline boolean& value::as<jsonxx::boolean>() {
-    // TODO(abdulras) throw an exception rather than asserting
-    assert(is<jsonxx::boolean>());
-    return boolean_;
+  // TODO(abdulras) throw an exception rather than asserting
+  assert(is<jsonxx::boolean>());
+  return boolean_;
 }
 
 template <>
 inline null& value::as<jsonxx::null>() {
-    // TODO(abdulras) throw an exception rather than asserting
-    assert(is<jsonxx::null>());
-    return null_;
+  // TODO(abdulras) throw an exception rather than asserting
+  assert(is<jsonxx::null>());
+  return null_;
 }
 
 bool allow_comments(void);
@@ -145,4 +157,3 @@ void parse(std::istream& stream, value& value);
 };
 
 #endif
-
